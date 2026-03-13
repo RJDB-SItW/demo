38635b73d4112cfcf2f738e4ced0cac13cc6ce06
RJDB-SItW <3465912722@qq.com>
2026-03-13 18:20:56 +0800

    第六次提交

--- src/main.cpp
+++ src/main.cpp
@@ -14,5 +14,4 @@ int main(int argc, char *argv[])
     std::cout << "git正常工作1" << std::endl;
     std::cout << "git正常工作2" << std::endl;
     std::cout << "git正常工作3" << std::endl;
-    std::cout << "git正常工作4" << std::endl;
 }
