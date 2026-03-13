d8988a1f732a8cc4ef10e926ca3db9059eaf5dbd
RJDB-SItW <3465912722@qq.com>
2026-03-13 18:57:34 +0800

    第八次提交

---
+++ proj1/src/main.cpp
@@ -0,0 +1,18 @@
+
+#include <iostream>
+#include <thread>
+
+void hello()
+{
+    std::cout << "线程跑起来了" << std::endl;
+}
+
+int main(int argc, char *argv[])
+{
+    std::thread t(hello);
+    t.join();
+    std::cout << "git正常工作1" << std::endl;
+    std::cout << "git正常工作2" << std::endl;
+    std::cout << "git正常工作3" << std::endl;
+    std::cout << "git正常工作4" << std::endl;
+}
